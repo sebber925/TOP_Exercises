fb5574a95c320538958245e85fd9912a2ebcfd14
sebber925 <63114738+sebber925@users.noreply.github.com>
2025-02-09 17:19:55 +0100

    Added exercise results

---
+++ Door Policy/script.js
@@ -0,0 +1,49 @@
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the front door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function frontDoorResponse(line) {
+  let firstLetter = line.charAt(0);
+  return firstLetter;
+}
+
+/**
+ * Format the password for the front-door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the front door password
+ */
+export function frontDoorPassword(word) {
+  let firstLetterPassword = word.charAt(0);
+  let restOfWord = word.slice(1);
+  let capitalFirstLetter = firstLetterPassword.toUpperCase();
+  return capitalFirstLetter + restOfWord.toLowerCase();
+}
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the back door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function backDoorResponse(line) {
+  let trimmedLine = line.trim();
+  let lastCharacter = trimmedLine.slice(-1);
+  return lastCharacter;
+}
+
+/**
+ * Format the password for the back door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the back door password
+ */
+export function backDoorPassword(word) {
+  return frontDoorPassword(word) + ", please"; 
+}
